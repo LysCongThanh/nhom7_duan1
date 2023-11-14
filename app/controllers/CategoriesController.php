01d79dea32d832791b1ac668721f39cc7687fda6
Th@2.dev <99267273+Webww@users.noreply.github.com>
2023-11-14 22:57:06 +0700

    Make CategoriesController
    - Show categories(list) page

--- app/controllers/CategoriesController.php
+++ app/controllers/CategoriesController.php
@@ -14,8 +14,17 @@ class CategoriesController extends Controller
     public function page() {
         $this->data['sub_content']['authors'] = $this->authors;
         $this->data['sub_content']['publishers'] = $this->publishers;
-        $this->data['sub_content']['publishers'] = $this->categories;
+        $this->data['sub_content']['categories'] = $this->categories;
+        $this->data['sub_content']['script_src'] = 'classification-list';
         $this->data['content'] = 'admin/categories/list';
         $this->render('layouts/admin_layout', $this->data);
+    }
+
+    public function add_categories() {
+        $request = new Request;
+
+        if($request->isPost()) {
+            echo 123;
+        }
     }
 }
