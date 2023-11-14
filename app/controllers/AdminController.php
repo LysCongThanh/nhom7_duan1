3d602e3f7cdedce0c3cb0ab4f0fd729f8afa2337
Th@2.dev <99267273+Webww@users.noreply.github.com>
2023-11-14 19:44:08 +0700

    Make AdminController

---
+++ app/controllers/AdminController.php
@@ -0,0 +1,25 @@
+<?php
+
+class AdminController extends Controller
+{
+    public $user, $data = [];
+    public function __construct()
+    {
+        $this->user = $this->model('UsersModel');
+    }
+
+    public function login()
+    {
+
+    }
+
+    public function logout()
+    {
+
+    }
+
+    public function page()
+    {
+
+    }
+}
