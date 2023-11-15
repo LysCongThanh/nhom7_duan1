1d50c7a39678d101d526667cd11875a87cf28cc3
TruongDan01 <danvtpc06576@fpt.edu.vn>
2023-11-15 19:41:43 +0700

    Create Product controller class

---
+++ app/controllers/ProductController.php
@@ -0,0 +1,77 @@
+<?php
+
+ class ProductController extends Controller 
+ {
+    public $products, $authors, $publishers, $categories, $data = [];
+
+    public function __construct()
+    {
+        $this->products = $this->model('ProductModel');
+        $this->categories = $this->model('CategoriesModel');
+    }
+
+    public function get_product()
+    {
+        $this->data['sub_content']['categories'] = $this->categories->getList();
+        $this->data['msg'] = Session::flash('msg');
+        $this->data['sub_content']['old'] = Session::flash('old');
+        $this->data['sub_content']['title'] = 'Thêm Sản Phẩm';
+        $this->data['content'] = 'admin/products/add';
+        $this->render('layouts/admin_layout', $this->data);
+    }
+
+    public function post_product()
+    {
+        $request = new Request();
+
+        if ($request->isPost()) {
+            $request->rules([
+                'name' => 'unique:books:name_book'
+            ]);
+
+            $request->messages([
+                'name.unique' => 'Sản phẩm đã có trong cửa hàng'
+            ]);
+            
+            $validate = $request->validate();
+            if (!$validate) {
+                Session::flash('msg', 'Đã có lỗi xảy ra vui lòng kiểm tra lại');
+                Session::flash('errors', $request->errors());
+                Session::flash('old', $request->getFields());
+              
+                $response  = new Response();
+                $response->redirect('them-san-pham');
+            }
+          
+            $data = $request->getFields();
+            $convertedData = array(
+                'id_category' => $data['category'],
+                'id_author' => $data['author'],
+                'id_publisher' => $data['publisher'],
+                'book_name' => $data['name'],
+                'describe_long' => $data['long-editor'],
+                'describe_short' => $data['sort-editor'],
+                'quantity_inventory' => $data['quantity'],
+                'price' => $data['price'],
+                'discount_price' => $data['sale_price'],
+                'publication_date' => $data['date_publication'],
+                'ISBN' => $data['ISBN'], 
+                'size' => $data['size'],
+                'num_page' => $data['number_pages']
+            );
+            $image = new DropZoneController();
+
+            $convertedData['slug'] = $this->generateSlug($convertedData['name']);
+            $rult = $this->products->insertProduct($convertedData); 
+
+            if (!$result) {
+                Session::flash('msg', 'Thêm sản phẩm thành công');
+                $response  = new Response();
+                $response->redirect('danh-sach-san-pham');
+                
+            }
+        }
+    }
+ }
+
+?>
