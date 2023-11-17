2d06122cce7ce678e97b3e27b2e7caa4b6e1d292
TruongDan01 <danvtpc06576@fpt.edu.vn>
2023-11-17 13:18:45 +0700

    Update DropZoneController

--- app/controllers/DropZoneController.php
+++ app/controllers/DropZoneController.php
@@ -2,13 +2,16 @@
 
 class DropZoneController extends Controller
 {
-    public $response;
+    public $response, $products, $images;
     public function __construct()
     {
         $this->response = new Response();
+        $this->images = $this->model('DropZoneModel');
+        $this->products = $this->model('ProductModel');
     }
     public function upLoadImage() 
-    {   $request = new Request();
+    {   
+        $request = new Request();
         $data = $request->getFields();
         $file = $data['image'];
         print_r($file);
@@ -40,7 +43,66 @@ class DropZoneController extends Controller
            $this->response->redirect('them-san-pham');
         }
 
+        $id_product = $this->products->getLatestId();
+
+        if ($id_product) {
+            $data = [
+                'id_book' => $id_product,
+                'name' => $new_file_name
+            ];
+            $this->images->insertImages($data);
+        }
         
+    }
+
+    public function upLoadImages() 
+    {
+        $request = new Request();
+        $data = $request->getFields();
+        $files = $data['album_images']; 
+        
+        $target_directory = 'public/uploads/products/' . date('Y_m') . '/';
+        
+        if (!is_dir($target_directory)) {
+            try {
+                mkdir($target_directory, 0755, true);
+            } catch (\Exception $e) {
+                $errors[] = $e->getMessage();
+                return false;
+            }
+        }
+        
+        $uploaded_file_names = []; // Mảng chứa tên các file đã được tải lên
+        
+        foreach ($files['tmp_name'] as $key => $tmp_name) {
+            $file_extension = strtolower(pathinfo($files['name'][$key], PATHINFO_EXTENSION));
+            $new_file_name = time() . '_' . uniqid() . '.' . $file_extension;
+            $new_file_path = $target_directory . $new_file_name;
+            
+            while (file_exists($new_file_path)) {
+                $new_file_name = time() . '_' . uniqid() . '.' . $file_extension;
+                $new_file_path = $target_directory . $new_file_name;
+            }
+            
+            if (!move_uploaded_file($tmp_name, $new_file_path)) {
+                Session::flash('errors', 'Tải lên không thành công');
+                $this->response->redirect('them-san-pham');
+            }
+            
+            $uploaded_file_names[] = $new_file_name; // Thêm tên file vào mảng uploaded_file_names
+        }
+        
+        $id_product = $this->products->getLatestId();
+        
+        if ($id_product) {
+            foreach ($uploaded_file_names as $file_name) {
+                $data = [
+                    'id_book' => $id_product,
+                    'name' => $file_name
+                ];
+                $this->images->insertImages($data);
+            }
+        }
     }
 }
 
