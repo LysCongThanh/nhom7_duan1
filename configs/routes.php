e62725854590f3c10170f476bd01fbadca663cbe
TruongDan01 <danvtpc06576@fpt.edu.vn>
2023-11-17 13:24:40 +0700

    Add routers

--- configs/routes.php
+++ configs/routes.php
@@ -1,5 +1,9 @@
 <?php
+// Admin Routings
 $routes['dashboard'] = 'admin/page';
 $routes['them-san-pham'] = 'product/get_product';
+$routes['danh-sach-san-pham'] = 'product/list';
+$routes['danh-sach-binh-luan'] = 'comments/list';
+$routes['phan-loai'] = 'classify/list';
 
-
+// Client Routings
