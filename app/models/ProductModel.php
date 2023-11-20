fcb7ce5742eeaa9ff6dbb463621171f7457743e1
TDev <congthanh1770@gmail.com>
2023-11-20 15:04:20 -0800

    Edit tableName, columnName

--- app/models/ProductModel.php
+++ app/models/ProductModel.php
@@ -15,7 +15,7 @@ class ProductModel extends Model
     }
     function primaryKey()
     {
-        return 'id_book';
+        return 'id';
     }
 
 
@@ -25,47 +25,4 @@ class ProductModel extends Model
         return $data;
     }
 
-    public function getLatestId()
-    {
-        $data = $this->db->query("SELECT MAX(id_book) AS latest_id FROM $this->_table")->fetch(PDO::FETCH_ASSOC);
-        return $data['latest_id'] + 1;
-    }
-
-    public function getListProducts()
-    {
-        $data = $this->db->select($this->_field)->table('products as p')->join('categories as c', 'p.category_id = c.id')->get();
-        return $data;
-    }
-
-    public function getListProductsCategories($id)
-    {
-        $data = $this->db->select($this->_field)->table('books as b')->join('categories as c', 'b.id_category = c.id_category')->where('b.id_category', '=', $id)->get();
-        return $data;
-    }
-
-    public function getDetailProduct($id)
-    {
-        $data = $this->db->table('books')->where('id_book', '=', $id)->first();
-        return $data;
-    }
-
-    public function insertProduct($data)
-    {
-        $this->db->table('books')->insert($data);
-    }
-
-    public function updateProduct($data, $id)
-    {
-        $this->db->table('products')->where('id', '=', $id)->update($data);
-    }
-
-    public function deleteProduct($id)
-    {
-        $this->db->table('products')->where('id', '=', $id)->delete();
-    }
-    public function count()
-    {
-        $data = $this->db->select('COUNT(id) As soLuong')->table('products')->get();
-        return $data;
-    }
 }
