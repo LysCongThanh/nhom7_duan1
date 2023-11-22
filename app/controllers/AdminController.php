4e590765e424dcff01373e7b6aa3c2573b8a5525
Duytt06458 <145202748+Duytt06458@users.noreply.github.com>
2023-11-22 08:19:11 +0700

    Update AdminCoonTroller

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -2,14 +2,20 @@
 
 class AdminController extends Controller
 {
-    public $user, $data = [];
+    public $user,$order,$product, $data = [];
     public function __construct()
     {
         $this->user = $this->model('UsersModel');
+        $this->order = $this->model('OrdersModel');
+        $this->product = $this->model('ProductModel');
     }
 
     public function index()
     {
+        $this->data['sub_content']['best_salers'] = $this->product->best_saler();
+        $this->data['sub_content']['count_discounts'] = $this->order->count_discount();
+        $this->data['sub_content']['count_order'] = $this->order->count_id();
+        $this->data['sub_content']['count_price'] = $this->order->count_totalPrice();
         $this->data['sub_content']['title'] = 'Trang Chủ';
         $this->data['content'] = 'admin/home/home';
         $this->render('layouts/admin_layout', $this->data);
