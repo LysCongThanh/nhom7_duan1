0e24e7095b44b71a4a3fc610ef2d03a70fc4999e
Duytt06458 <145202748+Duytt06458@users.noreply.github.com>
2023-11-22 08:20:17 +0700

    Update ProductModel - BestSaler

--- app/models/ProductModel.php
+++ app/models/ProductModel.php
@@ -25,4 +25,16 @@ class ProductModel extends Model
         return $data;
     }
 
+    //Top Sản Phẩm Bán Chạy
+    public function best_saler(){
+        $data = $this->db->select('b.id,b.book_name as name, b.views as view,cm.rating as comment, SUM(od.quantity) as total')
+        ->table('orders_detail as od')
+        ->join('books as b', 'b.id=od.book_id')
+        ->join('comments as cm', 'cm.book_id=b.id')
+        ->groupBy('b.id')
+        ->orderBy('total', 'DESC')
+        ->limit(5)->get();
+        return $data;
+    }
+
 }
