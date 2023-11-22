352b3f17d75e2294641117b16f643d86efc5b9ca
Duytt06458 <145202748+Duytt06458@users.noreply.github.com>
2023-11-22 08:21:05 +0700

    Update OrdersModel - CountTotalPrice - CountID - CountDiscount

--- app/models/OrdersModel.php
+++ app/models/OrdersModel.php
@@ -30,5 +30,25 @@ class OrdersModel extends Model {
         $data = $this->db->table('orders_detail')->where('id', '=', $id)->first();
         return $data;
     }
+
+    //Tổng doanh thu
+    public function count_totalPrice()
+    {
+        $data = $this->db->select('SUM(der.total_price) AS doanhthu')->table('orders AS der')->first();
+        return $data;
+    }
+
+    //Tổng giao dịch
+    public function count_id()
+    {
+        $data = $this->db->select('COUNT(der.id) AS giaodich')->table('orders AS der')->first();
+        return $data;
+    }
+
+    //Bán Phiếu Giảm Giá
+    public function count_discount(){
+        $data = $this->db->select('SUM(od.discount) as giamgia')->table('orders_detail as od')->first();
+        return $data;
+    }
 }
 ?>
