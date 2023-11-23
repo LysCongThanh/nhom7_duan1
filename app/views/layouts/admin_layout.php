97ba2ab33d333d099ecb45808f02872baae84c24
Duytt06458 <145202748+Duytt06458@users.noreply.github.com>
2023-11-23 20:53:44 +0700

    Update admin_layout

--- app/views/layouts/admin_layout.php
+++ app/views/layouts/admin_layout.php
@@ -4,5 +4,5 @@ $this->render('blocks/admin/sidenav');
 $this->render('blocks/admin/navbar', $sub_content);
 $this->render($content, $sub_content);
 $this->render('blocks/admin/setting');
-$this->render('blocks/admin/script');
+$this->render('blocks/admin/script', $sub_content);
 ?>
