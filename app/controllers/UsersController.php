236cfa431daac3ea83f8c617a8f8a55e15e7ea8e
Duytt06458 <145202748+Duytt06458@users.noreply.github.com>
2023-11-23 20:50:46 +0700

    Update UsersController

--- app/controllers/UsersController.php
+++ app/controllers/UsersController.php
@@ -12,8 +12,10 @@ class UsersController extends Controller{
 
     public function list()
     {
-        $this->data['sub_content']['count_ratings'] = $this->comment->ratings(); 
-        $this->data['sub_content']['count_orders'] = $this->order->count_id(); 
+        $this->data['sub_content']['script_src'] = 'chart_users';
+        $this->data['sub_content']['count_ratings'] = $this->comment->ratings();
+        $this->data['sub_content']['count_orders'] = $this->order->count_id();
+        $this->data['sub_content']['count_price'] = $this->order->count_totalPrice(); 
         $this->data['sub_content']['count_users'] = $this->users->count_users();        
         $this->data['sub_content']['users'] = $this->users->getListUsers();
         $this->data['sub_content']['title'] = 'Danh Sách Người Dùng';
