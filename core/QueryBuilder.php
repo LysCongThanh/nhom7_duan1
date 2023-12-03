645fc853483052f64d7f8042d1cddee773d651ae
TruongDan01 <danvtpc06576@fpt.edu.vn>
2023-12-03 21:07:41 +0700

    Require lirbary Infobip

--- core/QueryBuilder.php
+++ core/QueryBuilder.php
@@ -133,7 +133,7 @@ trait QueryBuilder
 
     public function first()
     {
-        $sqlQuery = "SELECT $this->selectField FROM $this->tableName $this->innerJoin $this->leftJoin $this->where $this->orderBy $this->groupBy $this->limit";
+        $sqlQuery = "SELECT $this->selectField FROM $this->tableName $this->innerJoin $this->leftJoin $this->where $this->groupBy $this->orderBy $this->limit";
         $query = $this->query($sqlQuery);
         $this->resetQuery();
         if (!empty($query)) return $query->fetch(PDO::FETCH_ASSOC);
@@ -143,8 +143,7 @@ trait QueryBuilder
     public function get()
     {
         // echo $this->innerJoin;
-        $sqlQuery = "SELECT $this->selectField FROM $this->tableName $this->innerJoin $this->leftJoin $this->where  $this->orderBy $this->groupBy $this->limit";
-        
+        $sqlQuery = "SELECT $this->selectField FROM $this->tableName $this->innerJoin $this->leftJoin $this->where $this->groupBy $this->orderBy $this->limit";
         $query = $this->query($sqlQuery);
         $this->resetQuery();
         if (!empty($query)) return $query->fetchAll(PDO::FETCH_ASSOC);
