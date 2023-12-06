c94dcf8b82104024a43ca24d2f8d481a2f18d6ae
TDev <congthanh1770@gmail.com>
2023-12-06 21:59:48 +0700

    refactor: Ghi lại các thao tác xử lí hình ảnh và gửi biểu mẫu
    Chi tiết:
    1. Khởi tạo Dropzone được customize lại và tối ưu hóa để tải lên hình ảnh.
    2. Cải thiện việc xử lý các hình ảnh hiện có trên csdl và các sự kiện xóa.
    2. Chi tiết: Xử lí ảnh chính(ImageMain) chỉ chứa được tồn tài 1 hình ảnh. Nếu có thao tác thêm mới(Drop & Drag image) thì xóa hình ảnh cũ và thêm mới.
    3. Gọi API để hiển thị album ảnh và xử lý dữ liệu nâng cao khi thêm hoặc xóa tệp.
    3. Chi tiết: Ghi lại các actions của dropzone(thumbnails) =>
    - Gọi API để hiển thị các hình ảnh của sản phẩm từ csdl. Nếu có => Show lên dropzone
    - Khi thêm mới hình ảnh(Drag & Drop image) ghi lại thao tác actions.action: 'add'
    - Khi xóa hình ảnh:
    + Trường hợp 1: Xóa hình ảnh cũ => ghi lại thao tác actions.action: 'remove'
    + Trường hợp 2: Xóa hình ảnh vừa thêm => Xóa các thao tác dư thừa.
    - Khi đóng popups dropzone => xóa tất cả các thao tác vừa lưu.
    - Khi click cập nhật => gửi resquest đến server xử lí các thao tác.
    - Gửi request thành công => tiến hành lưu dữ liệu cũ của form cập nhật sản phẩm
    - Response về trang chỉnh sửa sản phẩm và hiển thị dữ liệu cũ
    4. Thu thập và lưu trữ dữ liệu của trình soạn thảo Quill được sắp xếp hợp lý.
    6. Cải thiện việc xử lý lỗi và ghi nhật ký để gỡ lỗi tốt hơn.
    
    Feat: Cập nhật các thành phần biểu mẫu với dữ liệu JSON được lưu trữ
    
    Chi tiết:
    1. Đã sử dụng localStorage để duy trì và truy xuất dữ liệu biểu mẫu khi tải lại trang.
    2. Cập nhật các thành phần biểu mẫu với dữ liệu JSON được lưu trữ để mang lại trải nghiệm liền mạch cho người dùng.
    3. Khởi tạo trình soạn thảo Quill nâng cao với dữ liệu được lưu trữ.
    
    chore: Các cải tiến khác và sửa lỗi
    
    Chi tiết:
    1. Đã khắc phục sự cố liên quan đến tải tệp lên và gửi biểu mẫu không đồng bộ.
    2. Đã sửa URL điểm cuối API để khởi tạo Dropzone.
    3. Giải quyết các vấn đề về hàng đợi xử lý và xóa hình ảnh.
    4. Cải thiện thông báo lỗi và ghi nhật ký bảng điều khiển để gỡ lỗi tốt hơn.
    5. Dọn sạch các đoạn mã thừa hoặc không cần thiết.

--- public/assets/admin/js/pages/book-edit.js
+++ public/assets/admin/js/pages/book-edit.js
@@ -1,3 +1,26 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const storedJsonData = localStorage.getItem('jsonData');
+
+    if (storedJsonData) {
+        const jsonData = JSON.parse(storedJsonData);
+        console.log(jsonData);
+        const formEditProduct = document.querySelector('#form-edit-product').elements;
+
+        Array.from(formEditProduct).forEach((inputEle) => {
+            const fieldName = inputEle.name;
+            if (jsonData.hasOwnProperty(fieldName)) {
+                inputEle.value = jsonData[fieldName];
+            }
+        });
+
+        quillSortEditor.clipboard.dangerouslyPasteHTML(jsonData['sort-editor']);
+        quillLongEditor.clipboard.dangerouslyPasteHTML(jsonData['long-editor']);
+
+    }
+
+    localStorage.removeItem('jsonData');
+});
+
 Dropzone.autoDiscover = false;
 let existingFile = null;
 let myDropzone = new Dropzone("#image-dropzone", {
@@ -13,16 +36,21 @@ let myDropzone = new Dropzone("#image-dropzone", {
 
     init: function () {
         this.on("sending", function (file, xhr, formData) {
-            let query = new URLSearchParams(window.location.search);
             formData.append("id", query.get('id'));
+            formData.append("imageMain", 1);
         });
 
         this.on("addedfile", function (file) {
             // Check if there's already an existing file, and if so, remove it
-            if (existingFile) {
-               this.removeFile(existingFile);
 
-               existingFile = null;  // Reset the existing file
+            if (this.files.length > 1) {
+                this.removeFile(this.files[0]);
+            }
+
+            if (existingFile) {
+                this.removeFile(existingFile);
+
+                existingFile = null;  // Reset the existing file
             }
 
             fetchUpdatedData();
@@ -37,98 +65,164 @@ let myDropzone = new Dropzone("#image-dropzone", {
 
             this.displayExistingFile(
                 mockFile,
-                `http://localhost:33/public/uploads/products/2023_11/${mockFile.name}`
+                imagesOfProduct.imageMain.slug
             );
 
             existingFile = mockFile;  // Keep track of the existing file
         }
 
-            this.on("removedfile", function () {
-                let file = imagesOfProduct.imageMain;
+        this.on("removedfile", function () {
+            let file = imagesOfProduct.imageMain;
 
-                var xhttp = new XMLHttpRequest();
+            var xhttp = new XMLHttpRequest();
 
-                xhttp.open("POST", "http://localhost:33/dropzone/removeImage", true);
-                xhttp.setRequestHeader("Content-Type", "application/json");
-                xhttp.onreadystatechange = function () {
-                    if (xhttp.readyState == 4 && xhttp.status == 200) {
-                        // Xử lý phản hồi từ server
-                        console.log(xhttp);
-                    }
-                };
-                xhttp.send(JSON.stringify(file));
-            });
+            xhttp.open("POST", `${webRoot}/dropzone/removeImage`, true);
+            xhttp.setRequestHeader("Content-Type", "application/json");
+            xhttp.onreadystatechange = function () {
+                if (xhttp.readyState == 4 && xhttp.status == 200) {
+                    // Xử lý phản hồi từ server
+                    console.log(xhttp);
+                }
+            };
+            xhttp.send(JSON.stringify(file));
+        });
     }
 });
 
-
-
-
-
 let albumImagesDropzone = new Dropzone('#album-images-dropzone', {
-    url: "dropZone/addImages", // URL để gửi tệp đã chọn đến máy chủ
+    url: 'dropZone/addImages',
     paramName: 'album_images', // Tên của trường tệp trong yêu cầu POST
-    uploadMultiple: true, // Tải 1 lần nhiều file
     maxFilesize: 5, // Giới hạn dung lượng tệp (MB)
+    uploadMultiple: true,
     addRemoveLinks: true, // Hiển thị nút xóa cho từng tệp
     dictRemoveFile: '<i class="fa-solid fa-circle-xmark"></i>', // Chữ hoặc biểu tượng để xóa tệp
     dictDefaultMessage: '<i class="fas fa-cloud-upload-alt"></i> Drop files here or click to upload', // Tin nhắn mặc định
     acceptedFiles: "image/*", // Loại tệp cho phép (trong trường hợp này, chỉ hình ảnh)
-    autoProcessQueue: true, // Tắt tự động tải lên
+    autoProcessQueue: false, // Tắt tự động tải lên
     parallelUploads: 5, // Giảm số lượng tệp tin được tải lên cùng một lúc
 
     init: function () {
         const that = this;
-    
-        this.on('sending', function (file, xhr, formData) {
-            let query = new URLSearchParams(window.location.search);
-            formData.append("id", query.get('id'));
-        });
-    
+        let actions = [];
+
         if (imagesOfProduct.albumImages) {
             imagesOfProduct.albumImages.forEach((image) => {
                 let mockFile = {
                     name: image.name,
-                    size: 123
-                }
-    
-                that.displayExistingFile(mockFile, `http://localhost:33/public/uploads/products/2023_11/${mockFile.name}`);
-            });
-    
-            // Sự kiện 'removedfile' chung
-            that.on('removedfile', function (file) {
-                const imageDeleted = imagesOfProduct.albumImages.find(image => image.name === file.name);
-                if (imageDeleted) {
-                    const xhttp = new XMLHttpRequest();
-                    xhttp.open('POST', `${window.location.origin}/dropzone/removeImage`, true);
-                    xhttp.setRequestHeader("Content-Type", "application/json");
-                    xhttp.onreadystatechange = function () {
-                        if (xhttp.readyState == 4 && xhttp.status == 200) {
-                            console.log(xhttp);
-                            fetchUpdatedData();
-                        }
-                    };
-                    xhttp.send(JSON.stringify(imageDeleted));
-                }
+                    size: 123,
+                    id: image.id,
+                };
+
+                that.displayExistingFile(mockFile, `${webRoot}/${image.slug}`);
             });
         }
+
+        this.on("sending", function (file, xhr, formData) {
+            formData.append("id", query.get('id'));
+            formData.append("imageMain", 0);
+        });
+
+        this.on('addedfile', function (file) {
+            actions.push({ action: 'add', file: file });
+        });
+
+        this.on('removedfile', function (file) {
+            if (file.id) {
+                console.log(1);
+                actions.push({ action: 'remove', id: file.id });
+            } else {
+                actions = actions.filter(action => action.file !== file);
+            }
+
+            console.log(actions);
+        });
+
+        document.getElementById('cancelBtn').addEventListener('click', function () {
+            that.removeAllFiles(true);
+            if (imagesOfProduct.albumImages) {
+                imagesOfProduct.albumImages.forEach((image) => {
+                    let mockFile = {
+                        name: image.name,
+                        size: 123,
+                        id: image.id,
+                    };
+
+                    that.displayExistingFile(mockFile, `${webRoot}/${image.slug}`);
+                });
+            }
+            actions = [];
+        });
+
+        document.getElementById('updateBtn').addEventListener('click', function () {
+            actions.forEach(action => {
+                switch (action.action) {
+                    case 'remove':
+                        let id = action.id;
+                        const xhttp = new XMLHttpRequest();
+                        xhttp.open("POST", "http://localhost:33/dropzone/removeImage", true);
+                        xhttp.setRequestHeader("Content-Type", "application/json");
+                        xhttp.onreadystatechange = function () {
+                            if (xhttp.readyState == 4 && xhttp.status == 200) {
+                                console.log(xhttp);
+                            }
+                        };
+                        xhttp.send(JSON.stringify({ id }));
+                        break;
+                }
+            });
+
+            const formEditProduct = document.querySelector('#form-edit-product');
+            const sortDescriptionInput = document.getElementById('sort_description');
+            const longDescriptionInput = document.getElementById('long_description');
+
+            shortDescription = quillSortEditor.root.innerHTML;
+            longDescription = quillLongEditor.root.innerHTML;
+
+            sortDescriptionInput.value = shortDescription;
+            longDescriptionInput.value = longDescription;
+            const formData = new FormData(formEditProduct);
+            const jsonData = {};
+            formData.append('id', query.get('id'));
+            formData.append('request', `${webRoot}/sua-san-pham/id?${query.get('id')}`);
+
+            formData.forEach((value, key) => {
+                jsonData[key] = value;
+            });
+
+            const albumImagesDropzonePromise = new Promise((resolve) => {
+                albumImagesDropzone.on('queuecomplete', () => {
+                    resolve();
+                });
+                albumImagesDropzone.processQueue();
+            });
+
+            Promise.all([albumImagesDropzonePromise])
+                .then(() => {
+                    localStorage.setItem('jsonData', JSON.stringify(jsonData));
+                    location.reload();
+                })
+                .catch(error => {
+                    console.error('Lỗi khi gửi form hoặc Dropzones:', error);
+                });
+
+            
+        });
     }
+
 
 });
 
 function fetchUpdatedData() {
-    fetch('http://localhost:33/product/getImageAPI?id=3')
+    fetch(`${webRoot}/product/getImageAPI?id=${query.get('id')}`)
         .then(response => response.json())
         .then(data => {
-            // Update imagesOfProduct.imageMain with the new data
-            imagesOfProduct.imageMain = data
-            console.log(imagesOfProduct.imageMain)
-
-            // You can now use imagesOfProduct.imageMain with the updated data
+            imagesOfProduct.imageMain = data.image_main;
+            imagesOfProduct.albumImages = data.images;
+            console.log(imagesOfProduct.albumImages);
+            this.imageDeleted = data.images;
         })
         .catch(error => console.error('Error fetching updated data:', error));
 }
-
 
 Validator({
     form: '#form-edit-product',
@@ -136,15 +230,14 @@ Validator({
     errorSelector: '.form-message',
     rules: [
         Validator.isRequired('.input-group input[name="name"]', 'Không được bỏ trống'),
-        Validator.isRequired('.input-group input[name="sku"]',
-            '* Không được bỏ trống !'),
         Validator.isRequired('.input-group input[name="quantity"]',
             '* Vui lòng nhập số lượng sách'),
         Validator.isRequired('.input-group input[name="number_pages"]',
             '* Vui lòng nhập số trang sách'),
         Validator.isRequired('.input-group input[name="size"]',
             '* Vui lòng nhập kích thước sách'),
-        Validator.isNumber('.input-group input[name="price"]'),
+        Validator.isNumber('.input-group input[name="price"]',
+            '* Vui lòng nhập giá sách'),
         Validator.isDiscount(
             '.input-group input[name="sale_price"]',
             function () {
@@ -152,24 +245,10 @@ Validator({
             },
             '* Giảm giá nhỏ hơn giá gốc !'
         ),
-        Validator.isRequired('.input-group select[name="category"]',
-            '* Vui lòng chọn danh mục !'),
-        Validator.isRequired('.input-group select[name="author"]',
-            '* Vui lòng chọn tác giả !'),
-        Validator.isRequired('.input-group select[name="publisher"]',
-            '* Vui lòng chọn nhà xuất bản !'),
     ],
     onSubmit: function (data) {
 
-        // Call API
-        const sortDescriptionInput = document.getElementById('sort_description');
-        const longDescriptionInput = document.getElementById('long_description');
 
-        let sortDescription = quillSortEditor.root.innerHTML;
-        let longDescription = quillLongEditor.root.innerHTML;
-
-        sortDescriptionInput.value = sortDescription;
-        longDescriptionInput.value = longDescription;
 
     }
 });
