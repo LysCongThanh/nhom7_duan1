2f4795ebb55fdb9003606cc4ce58f0b98ce72583
Duytt06458 <145202748+Duytt06458@users.noreply.github.com>
2023-12-07 20:07:04 +0700

    Update wishlist.js

--- public/assets/client/js/page/wishlist.js
+++ public/assets/client/js/page/wishlist.js
@@ -1,22 +1,28 @@
-function sendData(event){
-    event.preventDefault();
+function sendData(event) {
+  event.preventDefault();
 
   var productId = event.target.id; // Lấy ID của thẻ được click
-    console.log(productId);
+  console.log(productId);
   var xhr = new XMLHttpRequest();
- var url = "cart/addWishList?productId=" + productId;
+  var url = "cart/addWishList?productId=" + productId;
   xhr.open("POST", url, true);
 
   xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 
+  xhr.onload = function () {
+      if (xhr.status === 200) {
+          var response = JSON.parse(xhr.responseText);
 
-  xhr.onload = function() {
-    if (xhr.status === 200) {
-      
-    }
+          if (response.success) {
+            alert("Đã xảy ra lỗi khi thêm sản phẩm vào mục yêu thích.");
+          } else {
+              alert("Sản phẩm đã được thêm vào mục yêu thích.");
+          }
+      }
   };
   xhr.send();
 }
+
 function confirmDelete(element) {
     var productId = element.getAttribute("id");
   console.log(productId);
