7de7b2c43bf1c842c775c45cad8051dce4064c5d
Duytt06458 <145202748+Duytt06458@users.noreply.github.com>
2023-12-07 20:06:52 +0700

    Update cart.js

--- public/assets/client/js/page/cart.js
+++ public/assets/client/js/page/cart.js
@@ -1,19 +1,26 @@
-function sendCartData(event){
-    event.preventDefault();
+function sendCartData(event) {
+  event.preventDefault();
 
   var productId = event.target.id; // Lấy ID của thẻ được click
-    console.log(productId);
+  console.log(productId);
   var xhr = new XMLHttpRequest();
- var url = "cart/addCart?productId=" + productId;
+  var url = "cart/addCart?productId=" + productId;
   xhr.open("POST", url, true);
 
   xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 
-
-  xhr.onload = function() {
-    if (xhr.status === 200) {
-      
-    }
+  xhr.onload = function () {
+      if (xhr.status === 200) {
+          var response = JSON.parse(xhr.responseText);
+          if (response.success) {
+              var confirmation = confirm("Sản phẩm đã được thêm vào giỏ hàng. Bạn có muốn xem giỏ hàng không?");
+              if (confirmation) {
+                  window.location.href = "gio-hang";
+              }
+          } else {
+              alert("Đã xảy ra lỗi khi thêm sản phẩm vào giỏ hàng.");
+          }
+      }
   };
   xhr.send();
 }
