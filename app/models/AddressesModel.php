291e1c91184d31a59c1e49ee5576f37c06636885
trietnmpc06220 <145202740+TrietNamePK12345@users.noreply.github.com>
2023-12-15 21:55:24 +0700

    Update AddressesModel.php

--- app/models/AddressesModel.php
+++ app/models/AddressesModel.php
@@ -18,4 +18,16 @@ class AddressesModel extends Model {
     {
         $this->db->table('addresses')->insert($data);
     }
+
+    public function updateAddress($data,$id)
+    {
+        $this->db->table('addresses')->where('user_id', '=', $id)->update($data);
+       
+    }
+
+    public function getAddresbyId($id)
+    {
+       $data = $this->db->select('address, specific_address')->table('addresses')->where('user_id', '=', $id)->get();
+       return $data;
+    }
 }
